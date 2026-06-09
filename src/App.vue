9585cd8e77d12d17bc838da3a8b47083dee23065
pepa992 <2117597604@qq.com>
2026-06-09 14:49:03 +0800

    Add App.vue root component

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-container">
+    <SociologyUniverse />
+  </div>
+</template>
+
+<script setup>
+import SociologyUniverse from './components/SociologyUniverse.vue'
+</script>
+
+<style scoped>
+.app-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
